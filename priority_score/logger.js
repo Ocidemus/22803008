37d39bd96dd46e5a44d04e110adf205e1a37a41e
ocidemus <codeofascendence@gmail.com>
2026-05-08 16:25:59 +0530

    added logger for priority score

---
+++ priority_score/logger.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const path = require('path');
+
+const LOG_FILE = path.join(__dirname, 'app.log');
+
+function log(level, message, meta = {}) {
+  const entry = {
+    timestamp: getTimestamp(),
+    level,
+    message,
+    ...(Object.keys(meta).length > 0 && { meta })
+  };
+  const line = JSON.stringify(entry) + '\n';
+  fs.appendFileSync(LOG_FILE, line);
+  const prefix = `[${entry.timestamp}] [${level}]`;
+  if (level === 'ERROR') {
+    process.stderr.write(`${prefix} ${message}\n`);
+  } else {
+    process.stdout.write(`${prefix} ${message}\n`);
+  }
+}
+function getTimestamp() {
+  return new Date().toISOString();
+}
+
+module.exports = {
+  info: (msg, meta) => log('INFO', msg, meta),
+  warn: (msg, meta) => log('WARN', msg, meta),
+  error: (msg, meta) => log('ERROR', msg, meta),
+  debug: (msg, meta) => log('DEBUG', msg, meta),
+};
